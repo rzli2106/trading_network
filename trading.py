2a93daea91a61b1e90b2d74a6cadcb1e778436ea
Richard Li <rzli2106@gmail.com>
2026-09-09 21:11:23 -0400

    Removed tickers for now and changed to daily returns instead of closing prices

--- trading.py
+++ trading.py
@@ -2,8 +2,16 @@
 import yfinance as yf
 import pandas as pd
 
-tickers = ["AMZN", "AAPL", "GOOGL", "MSFT", "TSLA"]
-print(tickers)
+#tickers = ["AMZN", "AAPL", "GOOGL", "MSFT", "TSLA"]
+#print(tickers)
 
 market_data = yf.download(tickers, start="2026-08-01", end="2026-09-01")
 print(market_data)
+
+closing_prices = market_data['Close']
+daily_returns = closing_prices.pct_change()
+
+
+#using pandas to calculate the correlation matrix
+correlation_matrix = daily_returns.corr()
+print(correlation_matrix)
