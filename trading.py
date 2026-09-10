fc6e47a50a358b3e7231915905690d8b6f89942f
Richard Li <rzli2106@gmail.com>
2026-09-09 21:04:37 -0400

    added API data

--- trading.py
+++ trading.py
@@ -1,2 +1,9 @@
+# Creating a stock network tracker using stock data and Python
+import yfinance as yf
+import pandas as pd
+
 tickers = ["AMZN", "AAPL", "GOOGL", "MSFT", "TSLA"]
 print(tickers)
+
+market_data = yf.download(tickers, start="2026-08-01", end="2026-09-01")
+print(market_data)
